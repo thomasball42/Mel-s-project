a16ba8c2f65db2175bdb2b94dd4ae99f16beaca2
Thomas Ball <thomasball355@googlemail.com>
2025-02-28 12:43:25 +0000

    Upload scripts

---
+++ count_points.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 27 17:29:16 2025
+
+@author: Thomas Ball
+"""
+
+import geopandas as gpd
+
+import pandas as pd
+import os
+import numpy as np
+import shapely as sh
+import tqdm 
+
+records = ["LIVING_SPECIMEN", "OBSERVATION", "HUMAN_OBSERVATION", "OCCURANCE", "MACHINE_OBSERVATION"]
+minYear = 1850
+
+dat_path = os.path.join("dat", "0000098-250225085111116.csv")
+vec_path = os.path.join("dat", "ne_50m_admin_0_countries", "ne_50m_admin_0_countries.shp")
+
+df = pd.read_csv(dat_path, sep = "\t", header = 0)
+
+dff = df[(df.basisOfRecord.isin(records))&((df.year > minYear) | (df.year.isna()))]
+dff = dff[~dff.decimalLatitude.isna()]
+# dff = dff[~dff.individualCount.isna()]
+
+vc = gpd.read_file(vec_path, layer = "ne_50m_admin_0_countries")
+
+dfx = pd.DataFrame()
+
+for idx, row in tqdm.tqdm(vc.iterrows(), total = len(vc)):
+    
+    iidx = len(dfx)
+    dfx.loc[iidx, "ISO_A3"] = row.ISO_A3
+    dfx.loc[iidx, "long_name"] = row.SOVEREIGNT
+    dfx.loc[iidx, "count"] = 0
+    
+    geom = row.geometry
+    
+    for d_idx, d_row in dff.iterrows():
+        
+        lat = d_row.decimalLatitude
+        lon = d_row.decimalLongitude
+
+        point = sh.geometry.Point(lon, lat)
+        
+        if point.within(geom):
+                
+            count = d_row.individualCount
+            if np.isnan(count):
+                count = 1
+            
+            dfx.loc[iidx, "count"] = dfx.loc[iidx, "count"] + count
+        
+
+dfx = dfx.groupby("long_name").sum()
+
+dfx.to_csv("data_out.csv")
---
+++ pie_chart.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 28 12:31:25 2025
+
+@author: tom
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv("data_out.csv")
+
+df = df[df["count"] > 0]
+
+df = df.sort_values("count", ascending = False)
+
+fig, ax = plt.subplots()
+
+for i, (idx, row) in enumerate(df.iterrows()):
+    
+    lname = row.long_name
+    count = row["count"]
+    
+    ax.bar(i, count, color = "g", alpha = 0.5)
+
+ax.set_xticks(np.arange(0, len(df), 1), labels = df.long_name, rotation = 90)
+
+fig.set_size_inches(8, 6)
+fig.tight_layout()   
